5bbb5cb0b1c95713e545b5b9d01de9036d2f95ea
Nadia <reyhani.ronak@gmail.com>
2019-11-21 19:56:29 +0800

    removed all the duplicated files

---
+++ popcornculture/src/components/atoms/button.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Button = ({ onButtonClick, ...props }) => {
+  return (
+    <button className="Button" onClick={onButtonClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
