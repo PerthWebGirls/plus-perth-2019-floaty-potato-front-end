8179474fa4b3c539bb195d52e49343c66c5c64fc
Nadia <reyhani.ronak@gmail.com>
2019-11-22 17:52:54 +0800

    nav bar added-Kebab menu in progress

--- popcornculture/src/components/atoms/textlink.js
+++ popcornculture/src/components/atoms/textlink.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const textLink = ({ onButtonClick, linkType, ...props }) => {
+const TextLink = ({ onButtonClick, linkType, ...props }) => {
   return <div onClick={onButtonClick}>{props.children}</div>;
 };
 
-export default textLink;
+export default TextLink;
