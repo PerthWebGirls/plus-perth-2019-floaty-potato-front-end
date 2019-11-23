b0dbad4d6dd6c234aacb3c77fd3cfec0b1be4433
Nadia <reyhani.ronak@gmail.com>
2019-11-23 11:01:50 +0800

    Footer

--- popcornculture/src/App.js
+++ popcornculture/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import MainPage from "./components/pages/MainPage";
 import NavBar from "./components/molecules/NavBar";
+import AppFooter from "./components/molecules/AppFooter";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <MainPage />
+      <AppFooter />
     </div>
   );
 }
