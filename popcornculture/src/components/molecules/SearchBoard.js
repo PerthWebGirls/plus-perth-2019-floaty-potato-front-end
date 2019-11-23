50ec04ebece68a00b4dcb97ac6516939cd713b9c
Nadia <reyhani.ronak@gmail.com>
2019-11-23 11:51:18 +0800

    Banner has been added

--- popcornculture/src/components/molecules/SearchBoard.js
+++ popcornculture/src/components/molecules/SearchBoard.js
@@ -1,62 +1,30 @@
-// import React, { Component } from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Button from "@material-ui/core/Button";
+import React, { Component } from "react";
+import TextInput from "../atoms/TextInput";
+import Button from "../atoms/Button";
 
-// const useStyles = makeStyles(theme => ({
-//   container: {
-//     display: "flex",
-//     flexWrap: "wrap"
-//   },
-//   textField: {
-//     marginLeft: theme.spacing(1),
-//     marginRight: theme.spacing(1),
-//     width: 200
-//   },
-//   button: {
-//     margin: theme.spacing(1)
-//   },
-//   input: {
-//     display: "none"
-//   }
-// }));
+const SearchBoard = props => {
+  //   const [searchValue, setSearchValue] = useState("");
 
-// const Search = (props) => {
-//   const [searchValue, setSearchValue] = useState("");
+  //   const handleSearchInputChanges = (e) => {
+  //     setSearchValue(e.target.value);
+  //   }
 
-//   const handleSearchInputChanges = (e) => {
-//     setSearchValue(e.target.value);
-//   }
+  //   const resetInputField = () => {
+  //     setSearchValue("")
+  //   }
 
-//   const resetInputField = () => {
-//     setSearchValue("")
-//   }
+  //   const callSearchFunction = (e) => {
+  //     e.preventDefault();
+  //     props.search(searchValue);
+  //     resetInputField();
+  //   }
 
-//   const callSearchFunction = (e) => {
-//     e.preventDefault();
-//     props.search(searchValue);
-//     resetInputField();
-//   }
-//   const classes = useStyles();
+  return (
+    <div>
+      <TextInput name="Search" value="Search for movie" />
+      <Button>Search</Button>
+    </div>
+  );
+};
 
-//   return (
-//     <form className={classes.container} noValidate autoComplete="off">
-//       <div>
-//         <TextField
-//           id="standard-search"
-//           label="Search field"
-//           type="search"
-//           className={classes.textField}
-//           value={searchValue}
-//           onChange={handleSearchInputChanges}
-//           margin="normal"
-//         />
-//         <Button onClick={callSearchFunction} variant="contained" className={classes.button}>
-//           SEARCH
-//         </Button>
-//       </div>
-//     </form>
-//   );
-// }
-
-// export default Search;
+export default SearchBoard;
