91deb5fbb85763940d2213cfcae027ec1de16a8a
Madelynn <maddx@live.com.au>
2019-11-30 11:00:48 +0800

    fixed merge problem in NavBar.js, need further editing

--- popcornculture/src/components/molecules/NavBar.js
+++ popcornculture/src/components/molecules/NavBar.js
@@ -1,26 +1,34 @@
 import React from "react";
 import TextLink from "../atoms/TextLink";
 import Button from "../atoms/Button";
-
+import Menu from "../molecules/Menu";
 import Icon from "../atoms/Icon";
 import "./NavBar.css"
-
-
+const NavBar = ({ onTextLinkClick, onIconClick, ...props }) => {
+  return (
+    <div className = "NavBar">
+      <div className = "NavLogo">
+        <img className = "Logo" src="" alt="" />
+        <TextLink linkType="Popcornculture" onButtonClick={onTextLinkClick}>
+          PopcornCulture
+        </TextLink>
+      </div>
+      
       <Menu onIconClick={onIconClick} />
       <Button>Log in</Button>
-=======
-      <div className = "NavMenu">
-        <div className="dropdown">  
-        <Icon className="dropbtn" onButtonClick={onTextLinkClick} />
+
+      <div className="NavMenu">
+        <div className="dropdown">
+          <Icon className="dropbtn" onButtonClick={onTextLinkClick} />
           <div className="dropdown-content">
             <a href="#">Profile</a>
             <a href="#">Watchlist</a>
             <a href="#">Logout</a>
           </div>
-        </div> 
+        </div>
         <Button className="LogIn">Log in</Button>
       </div>
-      
+
 
     </div>
   );
