522602af3c0b2626cd9500cf6ca18e75e4530dee
Madelynn <maddx@live.com.au>
2019-11-30 11:37:03 +0800

    update App.js to match Dev branch changes (trying to resolve merge conflict)

--- popcornculture/src/App.js
+++ popcornculture/src/App.js
@@ -1,17 +1,14 @@
-import React from "react";
-import "./App.css";
-import MainPage from "./components/pages/MainPage";
-import NavBar from "./components/molecules/NavBar";
-import Footer from "./components/organisms/Footer";
-import BannerBoard from "./components/organisms/BannerBoard";
+import React from 'react';
+import AppContainer from './containers/AppContainer';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import './style.css';
+
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <BannerBoard />
-      <MainPage />
-      <Footer />
-    </div>
+    <Router>
+      <AppContainer className="style" />
+    </Router>
   );
 }
+
 export default App;
