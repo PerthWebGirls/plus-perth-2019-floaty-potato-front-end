4c4b00df5d62915867d7e03ea51f3661f1da837e
Nadia <reyhani.ronak@gmail.com>
2019-12-01 10:53:59 +0800

    Drop down profile menu

--- popcornculture/src/components/molecules/Menu.js
+++ popcornculture/src/components/molecules/Menu.js
@@ -2,51 +2,48 @@ import React, { Component } from "react";
 import TextLink from "../atoms/TextLink";
 import Icon from "../atoms/Icon"
 
-
 class Menu extends Component {
+  container = React.createRef();
   constructor() {
     super();
     this.state = {
       showMenu: false,
     }
-    this.showMenu = this.showMenu.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
-
-  showMenu(event) {
+  handleButtonClick = (event) => {
     event.preventDefault();
-    this.setState({
-      showMenu: true,
+    this.setState(state => {
+      return {
+        showMenu: !state.showMenu,
+      };
     });
+  };
+  handleClickOutside = event => {
+    if (this.container.current && !this.container.current.contains(event.target)) {
+      this.setState({
+        open: false,
+      });
+    }
   }
-
   render() {
     return (
-      <>
-        <div>
-          <Icon onIconClick={this.showMenu} />
-          {
-            this.state.showMenu
-              ? (
-                <div className="menu">
-                  <TextLink linkType="Profile">
-                    Profile
-      </TextLink>
-                  <TextLink linkType="Watch List" onButtonClick={this.props.onTextLinkClick}>
-                    Watch List
-      </TextLink>
-                  <TextLink linkType="log out" onButtonClick={this.props.onTextLinkClick}>
-                    Log out
-      </TextLink>
-                </div>
-              )
-              : (
-                null
-              )
-          }
-
-
-        </div>
-      </>
+      <div className="container" ref={this.container}>
+        <Icon onIconClick={this.handleButtonClick} />
+        {this.state.showMenu && (
+          <div className="dropdown">
+            <TextLink linkType="Profile" onButtonClick={this.props.onTextLinkClick}>
+              Profile
+                </TextLink>
+            <TextLink linkType="Watch List" onButtonClick={this.props.onTextLinkClick}>
+              Watch List
+                </TextLink>
+            <TextLink linkType="log out" onButtonClick={this.props.onTextLinkClick}>
+              Log out
+                </TextLink>
+          </div>
+        )}
+      </div>
     );
   }
 }
