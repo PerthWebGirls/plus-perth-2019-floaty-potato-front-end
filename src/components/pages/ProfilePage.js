92dea51f00e502f256b7f469aa2c88f852a3eb6b
Maddy-89 <53515512+Maddy-89@users.noreply.github.com>
2019-12-10 08:27:33 +0800

    ProfilePage.css added

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -2,13 +2,16 @@ import React from 'react';
 import WatchList from '../organisms/WatchList';
 import AccountDetail from '../organisms/AccountDetail';
 import MainTemplate from '../templates/MainTemplate';
+import "./ProfilePage.css"
+
 const ProfilePage = ({ profileDetail, handleLogin, handleLogout, loggedIn, ...props }) => {
     return (
+        <div className="page-container">
         <MainTemplate handleLogin={handleLogin} handleLogout={handleLogout} loggedIn={loggedIn}>
             <AccountDetail accountDetail={profileDetail} />
             <WatchList watchList={profileDetail} />
         </MainTemplate>
-
+        </div>
     )
 
 
